f5e8267a2175d07026fd7da954d688885d2ad93c
Masud-Ndatsu <masudndatsu@gmail.com>
2024-06-20 11:58:56 +0100

    migrated from cra to vite

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BackTop } from "antd";
+import { Home, NoPage } from "./Pages";
+import { Navbar, Footer } from "./Components";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Recover from "./Pages/Recover";
+
+const App = () => {
+     return (
+          <div className=" bg-white ">
+               <BrowserRouter>
+                    <Navbar />
+                    <Routes>
+                         <Route path="/" element={<Home />} />
+                         <Route path="/login" element={<Login />} />
+                         <Route path="/register" element={<Register />} />
+                         <Route path="/recover" element={<Recover />} />
+                         <Route path="/*" element={<NoPage />} />
+                    </Routes>
+                    <Footer />
+               </BrowserRouter>
+
+               <BackTop />
+          </div>
+     );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
-  ],
-  theme: {
-    extend: {
-
-    },
-  },
-  plugins: [
-    require('flowbite/plugin')
-    
-  ],
-}
+     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+     theme: {
+          extend: {},
+     },
+     plugins: [],
+};
