a73f1f1731dc8d7daf2d4e10c680c5ee063e974a
Masud-Ndatsu <masudndatsu@gmail.com>
2024-06-23 00:33:52 +0100

    fix: updated anchor to Link tag to avoid page refresh

--- src/Components/ButtonYellow.jsx
+++ src/Components/ButtonYellow.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ButtonYellow = ({link, text}) => {
-  return (
-    <div>
-      <button>
-        <a
-          href={link}
-          className="block py-3 px-4 font-medium text-center text-white bg-amber-500  active:shadow-none rounded-full shadow md:inline"
-        >
-          {text}
-        </a>
-      </button>
-    </div>
-  );
+const ButtonYellow = ({ link, text, style }) => {
+     return (
+          <div style={{ ...style }}>
+               <button>
+                    <Link
+                         to={link}
+                         className="block py-3 px-4 font-medium text-center text-white bg-amber-500  active:shadow-none rounded-full shadow md:inline"
+                    >
+                         {text}
+                    </Link>
+               </button>
+          </div>
+     );
 };
 
 export default ButtonYellow;
