6263f2503427445e0e1f9d8b3f1661b660e93a63
Masud-Ndatsu <masudndatsu@gmail.com>
2024-06-23 00:34:43 +0100

    feat: Added cart page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { BackTop } from "antd";
-import { Home, NoPage, Products } from "./Pages";
+import { Cart, Home, NoPage, Products } from "./Pages";
 import { Navbar, Footer } from "./Components";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
@@ -22,6 +22,7 @@ const App = () => {
                          path="/products/:productId"
                          element={<ProductDetails />}
                     />
+                    <Route path="/cart" element={<Cart />} />
                     <Route path="/*" element={<NoPage />} />
                </Routes>
                <Footer />
