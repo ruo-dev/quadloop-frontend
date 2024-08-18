4c130f3c76af7d607b3a83104b7823de36f3e7e6
Masud Ndatsu <masudndatsu@gmail.com>
2024-08-18 15:30:48 +0100

    fix: fixed use checkout hook

--- src/hooks/Cart/useCheckoutCart.jsx
+++ src/hooks/Cart/useCheckoutCart.jsx
@@ -1,43 +1,43 @@
 import { useCallback } from "react";
-import axios from "axios";
 import { toast } from "react-toastify";
 import { defaultEnvOptions } from "../../utils/defaultEnvOptions";
+import api from "../../utils/api";
 
 export default function useCheckoutPayment() {
-     const env = defaultEnvOptions();
+  const env = defaultEnvOptions();
 
-     const handlePaystackPayment = useCallback(
-          async (payload) => {
-               const toastId = toast.loading("Initializing payment...");
-               const url = `${env.PAYMENT_URL}/paystack/pay`;
+  const handlePaystackPayment = useCallback(
+    async (payload) => {
+      const toastId = toast.loading("Initializing payment...");
+      const url = `${env.PAYMENT_URL}/paystack/pay`;
 
-               try {
-                    const response = await axios.post(url, payload);
+      try {
+        const response = await api().post(url, payload);
 
-                    toast.update(toastId, {
-                         render: "Redirecting to Paystack...",
-                         type: "success",
-                         isLoading: false,
-                         autoClose: 3000,
-                    });
+        toast.update(toastId, {
+          render: "Redirecting to Paystack...",
+          type: "success",
+          isLoading: false,
+          autoClose: 3000,
+        });
 
-                    return response.data.data.data;
-               } catch (error) {
-                    toast.update(toastId, {
-                         render: "Payment initialization error!",
-                         type: "error",
-                         isLoading: false,
-                         autoClose: 3000,
-                    });
+        return response.data.data.data;
+      } catch (error) {
+        toast.update(toastId, {
+          render: "Payment initialization error!",
+          type: "error",
+          isLoading: false,
+          autoClose: 3000,
+        });
 
-                    console.error("Payment initialization error:", error);
-                    return null;
-               }
-          },
-          [env.PAYMENT_URL]
-     );
+        console.error("Payment initialization error:", error);
+        return null;
+      }
+    },
+    [env.PAYMENT_URL]
+  );
 
-     return {
-          handlePaystackPayment,
-     };
+  return {
+    handlePaystackPayment,
+  };
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-     baseURL: import.meta.env?.VITE_API_URL,
-     headers: {
-          "Content-Type": "application/json",
-     },
+  baseURL: import.meta.env?.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Function to get the token from cookies
@@ -13,89 +13,88 @@ const getToken = () => Cookies.get("jwt");
 const getUserId = () => JSON.parse(localStorage.getItem("user")).id;
 
 // Function to set the token in cookies
-const setToken = (token) => Cookies.set("jwt", token, {sameSite: "Lax"});
+const setToken = (token) => Cookies.set("jwt", token, { sameSite: "Lax" });
 
 // Function to get the refresh token from cookies
 const getRefreshToken = () => Cookies.get("refreshToken");
 
 // Function to set the refresh token in cookies
 const setRefreshToken = (refreshToken) =>
-     Cookies.set("refreshToken", refreshToken, {sameSite: "Lax"});
+  Cookies.set("refreshToken", refreshToken, { sameSite: "Lax" });
 
 axiosInstance.interceptors.request.use(
-     (config) => {
-          const token = getToken();
-          if (token) {
-               config.headers.Authorization = `Bearer ${token}`;
-          }
-          return config;
-     },
-     (error) => Promise.reject(error)
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 axiosInstance.interceptors.response.use(
-     (response) => response,
-     (error) => {
-          if (
-               (error.response && error.response.status === 403) ||
-               error.response.data.message === "Access denied"
-          ) {
-               window.location.href = "/login";
-          }
-          return Promise.reject(error);
-     }
+  (response) => response,
+  (error) => {
+    if (
+      (error.response && error.response.status === 403) ||
+      error.response.data.message === "Access denied"
+    ) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
 );
 
 axiosInstance.interceptors.response.use(
-     (response) => response,
-     async (error) => {
-          const originalRequest = error.config;
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
 
-          if (
-               error.response.status === 401 ||
-               (error.response.data.message === "jwt expired" &&
-                    !originalRequest._retry)
-          ) {
-               originalRequest._retry = true;
-               try {
-                    const {
-                         data: { data },
-                    } = await axiosInstance.post("/api/v1/auth/refresh-token", {
-                         refresh_token: getRefreshToken(),
-                         user_id: getUserId(),
-                    });
-                    console.log("refresh token: ", data);
-                    setToken(data.accessToken);
-                    setRefreshToken(data.refreshToken);
-                    axiosInstance.defaults.headers.common[
-                         "Authorization"
-                    ] = `Bearer ${data.accessToken}`;
+    if (
+      error.response.status === 401 ||
+      (error.response.data.message === "jwt expired" && !originalRequest._retry)
+    ) {
+      originalRequest._retry = true;
+      try {
+        const {
+          data: { data },
+        } = await axiosInstance.post("/api/v1/auth/refresh-token", {
+          refresh_token: getRefreshToken(),
+          user_id: getUserId(),
+        });
+        console.log("refresh token: ", data);
+        setToken(data.accessToken);
+        setRefreshToken(data.refreshToken);
+        axiosInstance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${data.accessToken}`;
 
-                    return axiosInstance(originalRequest);
-               } catch (err) {
-                    window.location.href = "/login";
-               }
-          }
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        window.location.href = "/login";
+      }
+    }
 
-          return Promise.reject(error);
-     }
+    return Promise.reject(error);
+  }
 );
 
 const api = () => {
-     return {
-          get: async (url, options = {}) => {
-               return await axiosInstance.get(url, options);
-          },
-          post: async (url, data, options = {}) => {
-               return await axiosInstance.post(url, data, options);
-          },
-          put: async (url, data, options = {}) => {
-               return await axiosInstance.put(url, data, options);
-          },
-          delete: async (url, options = {}) => {
-               return await axiosInstance.delete(url, options);
-          },
-     };
+  return {
+    get: async (url, options = {}) => {
+      return await axiosInstance.get(url, options);
+    },
+    post: async (url, data, options = {}) => {
+      return await axiosInstance.post(url, data, options);
+    },
+    put: async (url, data, options = {}) => {
+      return await axiosInstance.put(url, data, options);
+    },
+    delete: async (url, options = {}) => {
+      return await axiosInstance.delete(url, options);
+    },
+  };
 };
 
 export default api;
