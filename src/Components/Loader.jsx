99c600624e92f5821616375e272a3128d547147a
Masud Ndatsu <masudndatsu@gmail.com>
2024-08-18 05:09:48 +0100

    fix: Made website mobile responsive

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Circles } from "react-loader-spinner";
+
+const Loader = ({ style }) => (
+  <div className="flex justify-center items-center h-screen z-50" {...style}>
+    <div className="w-60 h-60 md:w-[150px] md:h-[150px]">
+      <Circles
+        height={"100%"}
+        width={"100%"}
+        color="#0d9488"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  </div>
+);
+
+export default Loader;
